1af0a220bfb9266e0a434bc8b4734c8dc9a1e935
Arkadiusz Slowikowski <peon125@gmail.com>
2017-02-15 14:52:03 +0100

    15.02

--- multi/Assets/scripts/CharactersPic.cs
+++ multi/Assets/scripts/CharactersPic.cs
@@ -5,15 +5,16 @@ using UnityEngine.UI;
 
 public class CharactersPic : MonoBehaviour 
 {
-    Image img;
+    public Sprite[] picsFireChar;
+    public string[] descFireChar;
+    public Sprite[] picsFrostChar;
+    public string[] descFrostChar;
+    Sprite[,] pics;
+    string[,] desc;
 
-	void Start() 
+    void Start()
     {
-        img = gameObject.GetComponent<Image>();
-	}
-	
-    void ChangePicture(Sprite sprite)
-    {
-        img.sprite = sprite;
+        pics = new Sprite[2,4];
+        desc = new string[2,3];
     }
 }
